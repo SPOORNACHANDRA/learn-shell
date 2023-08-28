aee6f12b11d1f52a546d9547050ce21fb20fd83d
s poorna chandra <poornanaidu1@gmail.com>
2023-08-28 18:51:49 +0530

    echo command in project script

---
+++ 02-print.sh
@@ -0,0 +1,18 @@
+echo hello world
+
+#red          31
+#green        32
+#yellow       33
+# blue        34
+# magenta     35
+#cyan         36
+
+ # syntax
+
+#echo -e "\e[CODEmMESSAGE\e[0m"
+echo -e "\e[31mhello world\e[0m"
+echo -e "\e[32mhello world\e[0m"
+echo -e "\e[33mhello world\e[0m"
+echo -e "\e[34mhello world\e[0m"
+echo -e "\e[35mhello world\e[0m"
+echo -e "\e[36mhello world\e[0m"
