6e7e920262e4dfc1a37c000b4db4067d7ff8a07c
s poorna chandra <poornanaidu1@gmail.com>
2023-09-03 18:24:12 +0530

    variables

--- 03-vars.sh
+++ 03-vars.sh
@@ -1,2 +1,24 @@
+#variables
+
 a=10
 echo a is $a or ${a}
+
+#special variables
+#$0 - $N, $*,$#
+
+
+# substitution  variables
+# you want to store some output of command
+#Command substitution
+
+DATE=$(date) #——→ normal parantesis------>is widley used option
+#DATE=${date}————>access the variable
+echo today Date is $DATE
+#single parantesis for Command substitution
+
+
+#arthemetic substituion
+ADD=$((2+2))
+echo ADD of 2+2 = $ADD
+
+#double parantesis for Command substitution
